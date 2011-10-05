798b378d34eead193ba0bd656f0f6b9c37e8ccb4
Tim Perevezentsev <riffm2005@gmail.com>
2011-10-05 14:33:58 +0400

    init

---
+++ testalchemy.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+
+import types
+from sqlalchemy import event
+from sqlalchemy.orm import util
+
+__all__ = ['Sample', 'Restorable', 'ModelsHistory']
+
+
+class sample_property(object):
+    def __init__(self, method):
+        self.method = method
+        self.__doc__ = method.__doc__
+
+    def __get__(self, inst, cls):
+        if inst is None:
+            return self
+        result = self.method(inst)
+        inst.db.add(result)
+        method_name = self.method.__name__
+        inst.used_properties.add(method_name)
+        setattr(inst, method_name, result)
+        return result
+
+
+class SampleMeta(type):
+    def __init__(cls, cls_name, bases, namespace):
+        super(SampleMeta, cls).__init__(cls_name, bases, namespace)
+        decorated_methods = {}
+        for attr_name, attr_value in namespace.items():
+            if not (attr_name.startswith('_') or attr_name == 'create_all') \
+            and isinstance(attr_value, (types.FunctionType, types.LambdaType)):
+                decorated_methods[attr_name] = attr_value
+                setattr(cls, attr_name, sample_property(attr_value))
+        setattr(cls, '_decorated_methods', decorated_methods)
+
+
+class Sample(object):
+    __metaclass__ = SampleMeta
+
+    def __init__(self, db):
+        self.db = db
+        self.used_properties = set()
+
+    def create_all(self):
+        for attr_name, attr_value in self.__class__.__dict__.items():
+            if isinstance(attr_value, sample_property):
+                getattr(self, attr_name)
+        self.db.commit()
+
+
+class Restorable(object):
+    def __init__(self, session_maker):
+        self.session_maker = session_maker
+        self.history = {}
+        self.db = None
+
+    def __enter__(self):
+        event.listen(self.session_maker, 'after_flush', self.after_flush)
+        self.db = self.session_maker()
+        return self.db
+
+    def __exit__(self, type, value, traceback):
+        self.db.close()
+        db = self.session_maker()
+        db.autoflush = False
+        for cls, ident_set in self.history.items():
+            for ident in ident_set:
+                instance = db.query(cls).get(ident)
+                if instance is not None:
+                    db.delete(instance)
+        db.commit()
+        db.close()
+        event.Events._remove(self.session_maker, 'after_flush', self.after_flush)
+
+    def after_flush(self, db, flush_context, instances=None):
+        for instance in db.new:
+            cls, ident = util.identity_key(instance=instance)
+            self.history.setdefault(cls, set()).add(ident)
+
+
+class ModelsHistory(object):
+
+    def __init__(self, db):
+        self.db = db
+        self.created = set()
+        self.deleted = set()
+        self.updated = set()
+
+    def last(self, model_cls, mode):
+        assert mode in ('created', 'updated', 'deleted')
+        dataset = getattr(self, mode)
+        return set([item for item in dataset if isinstance(item, model_cls)])
+
+    def last_created(self, model_cls):
+        return self.last(model_cls, 'created')
+
+    def last_updated(self, model_cls):
+        return self.last(model_cls, 'updated')
+
+    def last_deleted(self, model_cls):
+        return self.last(model_cls, 'deleted')
+
+    def assert_(self, model_cls, ident=None, mode='created'):
+        dataset = self.last(model_cls, mode)
+        error_msg = 'No insatances of %s were %s' % (model_cls, mode)
+        assert dataset, error_msg
+        if ident is not None:
+            ident = ident if isinstance(ident, (tuple, list)) else (ident,)
+            item = [i for i in dataset \
+                    if util.identity_key(instance=i)[1] == ident]
+            assert item,'No insatances of %s with %r were %s' % (model_cls,
+                                                                 ident,
+                                                                 mode)
+            return item[0]
+        return dataset
+
+    def assert_created(self, model_cls, ident=None):
+        return self.assert_(model_cls, ident, 'created')
+
+    def assert_updated(self, model_cls, ident=None):
+        return self.assert_(model_cls, ident, 'updated')
+
+    def assert_deleted(self, model_cls, ident=None):
+        return self.assert_(model_cls, ident, 'deleted')
+
+    def clear(self):
+        self.created = set()
+        self.deleted = set()
+        self.updated = set()
+
+    def __enter__(self):
+        event.listen(self.db, 'after_flush', self._after_flush)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.clear()
+        event.Events._remove(self.db, 'after_flush', self._after_flush)
+
+    def _after_flush(self, db, flush_context, instances=None):
+        def identityset_to_set(obj):
+            return set(obj._members.values())
+        self.created = self.created.union(identityset_to_set(db.new))
+        self.updated = self.updated.union(identityset_to_set(db.dirty))
+        self.deleted = self.deleted.union(identityset_to_set(db.deleted))
---
+++ tests.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from testalchemy import Sample, Restorable, ModelsHistory
+from sqlalchemy import (
+        MetaData, Table, Column, String, Integer, ForeignKey,
+        create_engine, UniqueConstraint)
+from sqlalchemy.orm import relation, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+metadata = MetaData()
+Model = declarative_base(metadata=metadata, name='Model')
+
+
+class User(Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, default='')
+    roles = relation('Role', passive_deletes='all')
+
+
+class Category(Model):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), unique=True)
+    roles = relation('Role', secondary='roles_category', passive_deletes='all')
+
+
+class Role(Model):
+    __tablename__ = 'roles'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey(User.id, ondelete='CASCADE'), nullable=False)
+    user = relation(User)
+    categories = relation(Category, secondary='roles_category', passive_deletes='all')
+    smi_id = Column(ForeignKey('smi.id', ondelete='CASCADE'), nullable=False)
+    smi = relation('Smi')
+
+
+class Smi(Model):
+    __tablename__ = 'smi'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    roles = relation(Role, passive_deletes='all')
+
+
+roles_category = Table('roles_category', metadata, 
+    Column('role_id', ForeignKey(Role.id, ondelete='CASCADE'), nullable=False),
+    Column('category_id', ForeignKey(Category.id, ondelete='CASCADE'), nullable=False),
+    UniqueConstraint('role_id', 'category_id')
+)
+
+
+class PressSample(Sample):
+    def john(self):
+        return User(name='john')
+
+    def cat1(self):
+        return Category(name='cat1')
+
+    def cat2(self):
+        return Category(name='cat2')
+
+    def kp_editor(self):
+        return Role(user=self.john, smi=self.kp,
+                    categories=[self.cat1, self.cat2])
+
+    def kp(self):
+        return Smi(name='kp')
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        engine = create_engine('sqlite:///:memory:', echo=False)
+        metadata.create_all(engine)
+        self.Session = sessionmaker(bind=engine, autoflush=False)
+        self.session = self.Session()
+
+    def tearDown(self):
+        self.session.close()
+
+    def test_restorable_and_normal_behavior(self):
+        with Restorable(self.Session) as session:
+            sample = PressSample(session)
+            sample.create_all()
+            session.delete(sample.cat1)
+            session.commit()
+        self.assertEqual(self.session.query(User).all(), [])
+        self.assertEqual(self.session.query(Category).all(), [])
+        self.assertEqual(self.session.query(Role).all(), [])
+        self.assertEqual(self.session.query(Smi).all(), [])
+
+    def test_restorable_and_exceptional_behavior(self):
+        def exceptional_behavior():
+            with Restorable(self.Session) as session:
+                sample = PressSample(session)
+                sample.create_all()
+                session.delete(sample.john)
+                raise Exception('unwanted')
+                session.commit()
+        self.assertRaises(Exception, exceptional_behavior)
+        self.assertEqual(self.session.query(User).all(), [])
+        self.assertEqual(self.session.query(Category).all(), [])
+        self.assertEqual(self.session.query(Role).all(), [])
+        self.assertEqual(self.session.query(Smi).all(), [])
+
+        def test_models_history_init(self):
+            session = self.session
+            with ModelsHistory(session) as history:
+                self.assertEqual(history.created, set())
+                self.assertEqual(history.updated, set())
+                self.assertEqual(history.deleted, set())
+                self.assertEqual(history.last_created(User), set())
+                self.assertEqual(history.last_updated(User), set())
+                self.assertEqual(history.last_deleted(User), set())
+                self.assertRaises(AssertionError, history.assert_created, User)
+                self.assertRaises(AssertionError, history.assert_updated, User)
+                self.assertRaises(AssertionError, history.assert_deleted, User)
+
+    def test_models_history_created(self):
+        session = self.session
+        with ModelsHistory(session) as history:
+            user = User(name='test')
+            session.add(user)
+            session.commit()
+            self.assertEqual(history.created, set([user]))
+            self.assertEqual(history.updated, set())
+            self.assertEqual(history.deleted, set())
+            self.assertEqual(history.last_created(User), set([user]))
+            self.assertEqual(history.last_updated(User), set())
+            self.assertEqual(history.last_deleted(User), set())
+            self.assertEqual(history.assert_created(User), set([user]))
+            self.assertEqual(history.assert_created(User, user.id), user)
+            self.assertRaises(AssertionError, history.assert_updated, User)
+            self.assertRaises(AssertionError, history.assert_deleted, User)
+
+    def test_models_history_updated(self):
+        session = self.session
+        user = User(name='test')
+        session.add(user)
+        session.commit()
+        session.expire_all()
+        with ModelsHistory(session) as history:
+            user = session.query(User).get(user.id)
+            user.name = 'test 1'
+            session.commit()
+            self.assertEqual(history.created, set())
+            self.assertEqual(history.updated, set([user]))
+            self.assertEqual(history.deleted, set())
+            self.assertEqual(history.last_created(User), set())
+            self.assertEqual(history.last_updated(User), set([user]))
+            self.assertEqual(history.last_deleted(User), set())
+            self.assertRaises(AssertionError, history.assert_created, User)
+            self.assertEqual(history.assert_updated(User), set([user]))
+            self.assertEqual(history.assert_updated(User, user.id), user)
+            self.assertRaises(AssertionError, history.assert_deleted, User)
+
+    def test_models_history_deleted(self):
+        session = self.session
+        user = User(name='test')
+        session.add(user)
+        session.commit()
+        session.expire_all()
+        with ModelsHistory(session) as history:
+            user = session.query(User).get(user.id)
+            session.delete(user)
+            session.commit()
+            self.assertEqual(history.created, set())
+            self.assertEqual(history.updated, set())
+            self.assertEqual(history.deleted, set([user]))
+            self.assertEqual(history.last_created(User), set())
+            self.assertEqual(history.last_updated(User), set())
+            self.assertEqual(history.last_deleted(User), set([user]))
+            self.assertRaises(AssertionError, history.assert_created, User)
+            self.assertRaises(AssertionError, history.assert_updated, User)
+            self.assertEqual(history.assert_deleted(User), set([user]))
+            self.assertEqual(history.assert_deleted(User, user.id), user)
+
+
+if __name__ == '__main__':
+    unittest.main()
